17fd08f15576b077d75d4c5d723243deb388789d
Martin Pezzini Bachmann <martinpezbach@gmail.com>
2022-06-18 17:05:48 -0300

    Delete front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/test-app directory

--- front-end/bloco-14-testes-automatizados-com-react-testing-library/dia-2-rtl-mocks-e-inputs/test-app/service.test.js
+++
@@ -1,7 +0,0 @@
-const service = require("./service");
-
-test("testando a função randomNumber", () => {
-  service.randomNumber = jest.fn().mockReturnValue(10);
-
-  expect(service.randomNumber()).toBe(10);
-});
